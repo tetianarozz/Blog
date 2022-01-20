669346568c155b7024b8c22fd5c5d57535e8c483
tetianarozz <tatianarozputnia@gmail.com>
2022-01-20 17:37:06 +0200

    comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])  # знайти потрібну статтю методом find
-    @comment = @post.comments.create(comment_params)  # create метод асоціації has_many  для створення і збереження комента
+    @comment = @post.comments.create(comment_params)  # create метод асоціації has_many для створення і збереження комента
     redirect_to post_path(@post)  # перенаправити юзера на сторінку зі статтею за допомогою хелпера post_path.
     # викликає у контроллері PostController метод show, який відображає show.html.erb сторінку
   end
