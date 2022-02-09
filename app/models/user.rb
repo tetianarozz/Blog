965093790c0f6e8a3ac2e9050e8f1d0a982d4d7b
tetianarozz <tatianarozputnia@gmail.com>
2022-02-09 20:41:17 +0200

    Add Validates To Users first_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,5 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_145408) do
+ActiveRecord::Schema.define(version: 2022_02_09_183527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
